7e81eeb65c23efd1a107612556f6c3c370457a12
ls <qqlishuang@gmail.com>
2021-05-12 14:52:37 +0800

    plugin layer for pytorch to tensorrt by onnx

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <fstream>
 #include "yolov5.h"
+#include "SiLUPlugin.h"
 
 void initParams(common::params &inputparams){
     inputparams.BATCH_SIZE=1;
     inputparams.IMAGE_HEIGHT=640;
     inputparams.IMAGE_WIDTH=640;
-    inputparams.onnxPath="../yolov5_tensorrt/model/yolov5s.onnx";
-    inputparams.save_path="../yolov5_tensorrt/model/yolov5s.serialized";
+    // inputparams.onnxPath="../yolov5_tensorrt/model/yolov5s.onnx";
+    // inputparams.save_path="../yolov5_tensorrt/model/yolov5s.serialized";
+   inputparams.onnxPath="../yolov5_tensorrt/model/yolov5s_plugin.onnx";
+   inputparams.save_path="../yolov5_tensorrt/model/yolov5s_plugin.serialized";
 }
 
 int main()
 {
+//     auto creator = getPluginRegistry()->getPluginCreator("SiLU", "1");
+//     printf("success in getPluginCreator\n");
     common::params inputparams;
     initParams(inputparams);
     YOLOv5 YOLOv5(inputparams);
---
+++ plugin/SiLUPlugin.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TRT_SILU_PLUGIN_H
+#define TRT_SILU_PLUGIN_H
+#include "NvInfer.h"
+#include "kernel.h"
+// #include "plugin.h"
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace nvinfer1
+{
+namespace plugin
+{
+
+class SILU : public IPluginV2IOExt
+{
+public:
+    SILU();
+
+    SILU(const void* buffer, size_t length);
+
+    ~SILU() override = default;
+
+    int getNbOutputs() const override;
+
+    Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) override;
+
+    int initialize() override;
+
+    void terminate() override;
+
+    size_t getWorkspaceSize(int maxBatchSize) const override;
+
+    int enqueue(
+        int batchSize, const void* const* inputs, void** outputs, void* workspace, cudaStream_t stream) override;
+
+    size_t getSerializationSize() const override;
+
+    void serialize(void* buffer) const override;
+
+    bool supportsFormatCombination(int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) const override;
+
+    const char* getPluginType() const override;
+
+    const char* getPluginVersion() const override;
+
+    void destroy() override;
+
+    IPluginV2IOExt* clone() const override;
+
+    void setPluginNamespace(const char* pluginNamespace) override;
+
+    const char* getPluginNamespace() const override;
+
+    DataType getOutputDataType(int index, const nvinfer1::DataType* inputTypes, int nbInputs) const override;
+
+    bool isOutputBroadcastAcrossBatch(int outputIndex, const bool* inputIsBroadcasted, int nbInputs) const override;
+
+    bool canBroadcastInputAcrossBatch(int inputIndex) const override;
+
+    void attachToContext(
+            cudnnContext* cudnnContext, cublasContext* cublasContext, IGpuAllocator* gpuAllocator) override;
+
+    void configurePlugin(const PluginTensorDesc* in, int nbInput, const PluginTensorDesc* out, int nbOutput) override;
+
+    void detachFromContext() override;
+
+    int input_size_;
+
+private:
+    const char* mPluginNamespace;
+    // int mBatchDim;
+    pluginStatus_t SiLUInference_cpu(const int n, const float* input, float* output);
+
+};
+
+class SiLUPluginCreator : public IPluginCreator
+{
+public:
+    SiLUPluginCreator();
+
+    ~SiLUPluginCreator() override = default;
+
+    const char* getPluginName() const override;
+
+    const char* getPluginVersion() const override;
+
+    const PluginFieldCollection* getFieldNames() override;
+
+    IPluginV2IOExt* createPlugin(const char* name, const PluginFieldCollection* fc) override;
+
+    IPluginV2IOExt* deserializePlugin(const char* name, const void* serialData, size_t serialLength) override;
+
+    void setPluginNamespace(const char* libNamespace) override
+    {
+        mNamespace = libNamespace;
+    }
+
+    const char* getPluginNamespace() const override
+    {
+        return mNamespace.c_str();
+    }
+
+
+private:
+    std::string mNamespace;
+    static PluginFieldCollection mFC;
+    static std::vector<PluginField> mPluginAttributes;
+};
+
+REGISTER_TENSORRT_PLUGIN(SiLUPluginCreator);
+
+} // namespace plugin
+} // namespace nvinfer1
+
+#endif // TRT_SILU_PLUGIN_H

c63b993fd07b29f6d736234de8d125626a7940c8
lishuang <qqlishuang@gmail.com>
2022-05-20 12:16:46 +0800

    update tensor class

--- yolov5_trt_new/main.cpp
+++ yolov5_trt_new/main.cpp
@@ -1,5 +1,6 @@
 #include "src/module/builder/trt_builder.h"
 #include "src/module/core/trt_tensor.h"
+#include "src/module/common/ilogger.h"
 #include <cuda_runtime.h>
 
 using namespace TRT;
@@ -27,6 +28,37 @@ static void test_tensor1(){
     ///////////////////////////////////////////////////////////////////
 }
 
+static void test_tensor2(){
+
+    ///////////////////////////////////////////////////////////////////
+    /* 内存的自动复制，依靠head属性标记数据最新的位置
+       若访问的数据不是最新的，则会自动发生复制操作 */
+    TRT::Tensor tensor({1, 3, 5, 5},nullptr);
+    INFO("tensor.head = %s", TRT::data_head_string(tensor.head()));   /* 输出 Init，内存没有分配 */
+
+    tensor.cpu<float>()[0] = 512;               /* 访问cpu时，分配cpu内存 */
+    INFO("tensor.head = %s", TRT::data_head_string(tensor.head()));   /* 输出 Host */
+
+    float* device_ptr = tensor.gpu<float>();    /* 访问gpu时，最新数据在Host，发生复制动作并标记最新数据在Device */
+    INFO("tensor.head = %s", TRT::data_head_string(tensor.head()));   /* 输出 Device */
+    //INFO("device_ptr[0] = %f", device_ptr[0]);                        /* 输出 512.00000，由于gpu内存修改为cudaMalloc，这里无法直接访问 */
+}
+
+static void test_tensor3(){
+
+    ///////////////////////////////////////////////////////////////////
+    /* 计算维度的偏移量 */
+    TRT::Tensor tensor({1, 3, 5, 5, 2, 5},nullptr);
+    auto ptr_origin   = tensor.cpu<float>();
+    auto ptr_channel2 = tensor.cpu<float>(0, 2, 3, 2, 1, 3);
+
+    INFO("Offset = %d", ptr_channel2 - ptr_origin);                          /* 输出678 */
+    INFO("Offset = %d", tensor.offset(0, 2, 3, 2, 1, 3));                    /* 输出678 */
+
+    int offset_compute = ((((0 * 3 + 2) * 5 + 3) * 5 + 2) * 2 + 1) * 5 + 3;  
+    INFO("Compute = %d", offset_compute);                                    /* 输出678 */
+}
+
 static void lesson1(){
     std::string onnx_file = "weights/yolov5n.onnx";
     std::string engine_file = "weights/yolov5n.engine";
@@ -41,5 +73,7 @@ int main(){
     
     // lesson1();
     test_tensor1();
+    test_tensor2();
+    test_tensor3();
     return 0;
 }
